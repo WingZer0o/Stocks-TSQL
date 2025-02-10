3e3ff3a483e3d54f1e87614beaa571337502c335
Mike Mulchrone <mikemulchrone987@gmail.com>
2025-02-09 20:04:59 -0500

    Create GetTickers.sql
    Get regular tickers

---
+++ Stocks/GetTickers.sql
@@ -0,0 +1,32 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Procedure (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- This block of comments will not be included in
+-- the definition of the procedure.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Mike Mulchrone>
+-- Create date: <2/9/2025>
+-- Description:	<Gets all tickers in the database>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetTickers]
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT Ticker FROM Tickers;
+END
+GO
